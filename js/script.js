e21468085d1dfbd8b8105fccf7c99191a7fee16b
Byron Chang and Natalia Telpukhova <natalia@>
2017-12-04 09:34:59 -0800

    refactor the js code

--- js/script.js
+++ js/script.js
@@ -1,19 +1,12 @@
 $(document).ready(function() {
   $("#formOne").submit(function() {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput= $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
-
-    $(".person1").append(person1Input);
-    $(".person2").append(person2Input);
-    $(".animal").append(animalInput);
-    $(".exclamation").append(exclamationInput);
-    $(".verb").append(verbInput);
-    $(".noun").append(nounInput);
-    $("#story").show();
     event.preventDefault();
+
+    var inputs = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+    inputs.forEach(function(input) {
+      var temp = $("input#" + input).val();
+      $( "." + input).append(temp)
+    });
+    $("#story").show();
   });
 });
